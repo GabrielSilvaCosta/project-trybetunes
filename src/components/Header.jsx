646dcc300967f23c5001d2f993d7262b5100014b
GabrielSilvaCosta <bob@>
2023-04-13 18:25:36 -0300

    requisito 4 feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends Component {
@@ -18,7 +19,7 @@ class Header extends Component {
       const users = await getUser();
       this.setState({ userName: users.name, loading: false });
     } catch (error) {
-      console.error('Erro ao recuperar usuario:', error);
+      console.error('Erro ao recuperar usuário:', error);
       this.setState({ loading: false });
     }
   }
@@ -26,16 +27,21 @@ class Header extends Component {
   render() {
     // estado atualizado
     // vamos renderizar e atualizar o loading
-    // se for antendida recebe carregando
-    // se nao userName não encontrado
+    // se for atendido recebe "Carregando..."
+    // se não userName não encontrado
     const { userName, loading } = this.state;
     return (
       <div data-testid="header-component">
         {loading ? (
           'Carregando...'
         ) : (
-          <p data-testid="header-user-name">{`${userName} usuario invalido`}</p>
+          <p data-testid="header-user-name">{`${userName} usuário inválido`}</p>
         )}
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Pesquisa</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Musicas Favoritas</Link>
+          <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+        </nav>
       </div>
     );
   }
