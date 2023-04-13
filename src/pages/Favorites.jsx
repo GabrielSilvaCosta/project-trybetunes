7c328f4ce7713e7f67f8e98ba3d6507f1ce2ac77
GabrielSilvaCosta <bob@>
2023-04-13 15:43:14 -0300

    requisito 1 feito

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-favorites" />
+
+    );
+  }
+}
+
+export default Album;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile">
+        {/* Conteúdo do componente Profile */}
+      </div>
+    );
+  }
+}
+
+export default Profile;
